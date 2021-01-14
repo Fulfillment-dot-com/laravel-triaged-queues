989c5231faaa4b42d3a017467faf0e6230301895
FoxxMD <FoxxMD@users.noreply.github.com>
2021-01-14 15:20:27 -0500

    Refactor and remove most functionality
    * Laravel 5.5 implements many features that make custom jobs obsolete such as before/after/exception events
    * More code inhibits compatibility, reduce surface as much as possible to make future upgrades easier
    * Remove touch on beanstalkd job since pheanstalk is now publicly accessible from the job
    * Add illuminate dependencies

--- src/Queue/Connectors/BeanstalkdConnector.php
+++ src/Queue/Connectors/BeanstalkdConnector.php
@@ -3,12 +3,7 @@
 namespace Fulfillment\TriagedQueues\Queue\Connectors;
 
 use Fulfillment\TriagedQueues\Exceptions\NoHostException;
-use Fulfillment\TriagedQueues\Queue\Queues\BetterBeanstalkdQueue;
 use Illuminate\Queue\BeanstalkdQueue;
-use Illuminate\Queue\Connectors\ConnectorInterface;
-use Illuminate\Support\Arr;
-use Pheanstalk\Pheanstalk;
-use Pheanstalk\PheanstalkInterface;
 use Log;
 
 class BeanstalkdConnector extends \Illuminate\Queue\Connectors\BeanstalkdConnector
@@ -22,35 +17,20 @@ class BeanstalkdConnector extends \Illuminate\Queue\Connectors\BeanstalkdConnect
      */
     public function connect(array $config)
     {
-        // get all config keys with 'host' in it
-        $hosts = array_filter(array_keys($config), function($key) { return strpos($key, 'host') !== false;});
         $maxAttempts = isset($config['attempts']) ? $config['attempts'] : 1;
+	    $staggerAttempts = isset($config['staggerAttempts']) ? $config['staggerAttempts'] : 0;
 
-        // sort by numeric ending so we tier hosts correctly
-        natsort($hosts);
+	    /** @var BeanstalkdQueue $queue */
+	    $queue    = parent::connect($config);
+	    $attempts = 0;
+	    while($attempts < $maxAttempts) {
+		    if($queue->getPheanstalk()->getConnection()->isServiceListening()) { // found a working host
+			    return $queue;
+		    }
+		    sleep($staggerAttempts);
+		    $attempts++;
+	    }
 
-        foreach($hosts as $host) {
-            $pheanstalk = new Pheanstalk($config[$host], Arr::get($config, 'port', PheanstalkInterface::DEFAULT_PORT), Arr::get($config, 'socketTimeout'));
-
-            // test pheanstalk to see if we have a connection
-            $attempts = 0;
-            while($attempts < $maxAttempts) {
-                if($pheanstalk->getConnection()->isServiceListening()) { // found a working host
-                    $queue = new BetterBeanstalkdQueue(
-                        $pheanstalk, $config['queue'], Arr::get($config, 'ttr', Pheanstalk::DEFAULT_TTR)
-                    );
-                    if(null !== $job = Arr::get($config, 'job')) {
-						$queue->setCustomPayloadJob($job);
-                    }
-	                return $queue;
-                } else {
-                    $attempts++;
-                }
-            }
-            Log::warning('Beanstalk host was unreachable.', ['beanstalkHost' => $config[$host], 'beanstalkAttempts' => $maxAttempts]);
-        }
-
-        // no working service found!
-        throw new NoHostException('No working host found for beanstalkd driver.');
+	    throw new NoHostException('Beanstalk host was unreachable');
     }
 }
--- src/TriagedQueueServiceProvider.php
+++ src/TriagedQueueServiceProvider.php
@@ -5,9 +5,6 @@ namespace Fulfillment\TriagedQueues;
 
 
 use Fulfillment\TriagedQueues\Queue\Connectors\BeanstalkdConnector;
-use Fulfillment\TriagedQueues\Queue\Connectors\RedisConnector;
-use Fulfillment\TriagedQueues\Queue\Connectors\SqsConnector;
-use Fulfillment\TriagedQueues\Queue\Connectors\SyncConnector;
 use Fulfillment\TriagedQueues\Queue\QueueManager;
 use Illuminate\Queue\QueueServiceProvider;
 
@@ -15,18 +12,11 @@ class TriagedQueueServiceProvider extends QueueServiceProvider
 {
     protected function registerManager()
     {
-        $this->app->singleton('queue', function ($app) {
-
-            $manager = new QueueManager($app);
-
-            $this->registerConnectors($manager);
-
-            return $manager;
-        });
-
-        $this->app->singleton('queue.connection', function ($app) {
-            return $app['queue']->connection();
-        });
+	    $this->app->singleton('queue', function ($app) {
+		    return tap(new QueueManager($app), function ($manager) {
+			    $this->registerConnectors($manager);
+		    });
+	    });
     }
 
     protected function registerBeanstalkdConnector($manager)
@@ -35,27 +25,4 @@ class TriagedQueueServiceProvider extends QueueServiceProvider
             return new BeanstalkdConnector();
         });
     }
-
-	protected function registerSyncConnector($manager)
-	{
-		$manager->addConnector('sync', function () {
-			return new SyncConnector;
-		});
-	}
-
-	protected function registerRedisConnector($manager)
-	{
-		$app = $this->app;
-
-		$manager->addConnector('redis', function () use ($app) {
-			return new RedisConnector($app['redis']);
-		});
-	}
-
-	protected function registerSqsConnector($manager)
-	{
-		$manager->addConnector('sqs', function () {
-			return new SqsConnector;
-		});
-	}
 }
